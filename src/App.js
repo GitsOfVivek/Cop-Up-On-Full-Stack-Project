96533ad0e6864237be5993abe86cc8c5d3cb501c
GitsOfVivek <singhvivek1999@yahoo.com>
2023-01-04 12:56:39 +0530

    homepage created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-// import logo from './logo.svg';
 import './App.css';
+import image from './img.png';
 
 function App() {
 	return (
-		<div className="App">
-			<h1>Hello World</h1>
-		</div>
+		<>
+			<div className="nav">
+				<div className="logo">Cop-Up-On</div>
+				<div className="nav-menu">
+					<ul>
+						<li>
+							<a href="#">About</a>
+						</li>
+						<li>
+							<a href="#">Contact</a>
+						</li>
+					</ul>
+				</div>
+				<div className="sign-in-btn">
+					<button>Sign In</button>
+				</div>
+			</div>
+			<div className="home">
+				<div className="home-left">
+					<div>
+						<button>Get Started</button>
+						<p>Save More with Our Exclusive Coupon</p>
+					</div>
+				</div>
+				<div className="home-right">
+					<img src={image} alt="image" />
+				</div>
+			</div>
+		</>
 	);
 }
 
