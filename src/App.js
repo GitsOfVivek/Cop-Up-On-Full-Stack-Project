a5a03bfa1fa8c2638c3832c4dccd3ecdb7399404
GitsOfVivek <singhvivek1999@yahoo.com>
2023-01-05 00:22:59 +0530

    convert functional comp to class comp

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
 import './App.css';
 import image from './img.png';
+import React, { Component } from 'react';
 
-function App() {
-	return (
-		<>
-			<div className="nav">
-				<div className="logo">Cop-Up-On</div>
-				<div className="nav-menu">
-					<ul>
-						<li>
-							<a href="#">About</a>
-						</li>
-						<li>
-							<a href="#">Contact</a>
-						</li>
-					</ul>
-				</div>
-				<div className="sign-in-btn">
-					<button>Sign In</button>
-				</div>
-			</div>
-			<div className="home">
-				<div className="home-left">
-					<div>
-						<button>Get Started</button>
-						<p>Save More with Our Exclusive Coupon</p>
+class App extends Component {
+	render() {
+		return (
+			<>
+				<div className="nav">
+					<div className="logo">Cop-Up-On</div>
+					<div className="nav-menu">
+						<ul>
+							<li>
+								<a href="#">About</a>
+							</li>
+							<li>
+								<a href="#">Contact</a>
+							</li>
+						</ul>
+					</div>
+					<div className="sign-in-btn">
+						<button>Sign In</button>
 					</div>
 				</div>
-				<div className="home-right">
-					<img src={image} alt="image" />
+				<div className="home">
+					<div className="home-left">
+						<div>
+							<button>Get Started</button>
+							<p>Save More with Our Exclusive Coupon</p>
+						</div>
+					</div>
+					<div className="home-right">
+						<img src={image} alt="image" />
+					</div>
 				</div>
-			</div>
-		</>
-	);
+			</>
+		);
+	}
 }
 
 export default App;
